abdde0f9909490e9b07f4650fdf4d0a95c26d261
11pzzz <nadhifzalmani@gmail.com>
2025-05-27 08:59:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.image("BC048F02-5A9D-4FB7-9FB9-BA5759CC84BB.jpeg",width=500)
 
 st.title("1pzz app")
-st.header("Mengecek Bilangan Ganjil Dan Genap”)
-angka = st.number_input(“Tulis Sebuah Angka:”, value= 0, step= 1) 
+st.header("Mengecek bilangan Ganjil dan Genap")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
 
 
 if(angka % 2) == 0:
